350b509e4f14bc7011aa33e38aa19a331ad226ef
Anne Havunen <anne.havunen@helsinki.fi>
2025-01-26 14:16:02 +0200

    Create project and tokenizer

---
+++ src/compiler/tokenizer.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+import re
+from typing import Literal
+
+@dataclass
+class SourceLocation:
+    file: str
+    line: int
+    column: int
+
+# for testing purposes
+L = SourceLocation(file="arbitrary_file", line=-99, column=-99)
+
+TokenType = Literal["int_literal", "identifier", "operator", "punctuation", "end"]
+
+@dataclass(frozen=True)
+class Token:
+    loc: SourceLocation
+    type: TokenType
+    text: str
+    
+    def __eq__(self, other: object) -> bool:
+        if self is other:
+            return True
+
+        if not isinstance(other, Token):
+            return False
+
+        if ((self.loc == other.loc or self.loc == L or other.loc == L) and
+            self.type == other.type and
+            self.text == other.text):            
+            return True
+
+        return False
+
+def tokenize(source_code: str, file_name: str = "file_name") -> list[Token]:
+    whitespace_re = re.compile(r'\s+')
+    integer_re = re.compile(r'[0-9]+')
+    identifier_re = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
+    operator_re = re.compile(r'==|!=|<=|>=|[-+*/=<>]')
+    punctuation_re = re.compile(r'[(){},;]')
+    comment_re = re.compile(r'//.*|#.*')
+
+    position = 0
+    line = 0
+    column = 0
+    result: list[Token] = []
+
+    def append_token(token_type: TokenType, text: str) -> None:
+        nonlocal position
+
+        location = SourceLocation(file_name, line, column)
+        result.append(Token(
+            loc=location,
+            type=token_type,
+            text=text
+        ))
+        position += len(text)
+
+    while position < len(source_code):
+        match = comment_re.match(source_code, position)
+        if match is not None:
+            position = match.end()
+            continue
+
+        match = whitespace_re.match(source_code, position)
+        if match is not None:
+            whitespaces = match.group()
+            for whitespace in whitespaces:
+                if whitespace == "\n":
+                    line += 1
+                    column = 0
+                elif whitespace == "\t":
+                    column += 8
+                else:
+                    column += 1
+
+            position = match.end()
+            continue
+
+        match = identifier_re.match(source_code, position)
+        if match is not None:
+            append_token("identifier", source_code[position:match.end()])
+            continue
+
+        match = integer_re.match(source_code, position)
+        if match is not None:
+            append_token("int_literal", source_code[position:match.end()])
+            continue
+
+        match = operator_re.match(source_code, position)
+        if match is not None:
+            append_token("operator", source_code[position:match.end()])
+            continue
+
+        match = punctuation_re.match(source_code, position)
+        if match is not None:
+            append_token("punctuation", source_code[position:match.end()])
+            continue
+        
+        raise Exception(f'Tokenization failed near {source_code[position:(position + 10)]}...')
+
+    return result
---
+++ tests/tokenizer_test.py
@@ -0,0 +1,110 @@
+from compiler.tokenizer import Token, tokenize, SourceLocation
+
+
+def test_tokenizer_basics() -> None:
+    L = SourceLocation(file="arbitrary_file", line=-99, column=-99)
+
+    assert tokenize("hello") == [
+        Token(loc=L, type='identifier', text='hello')
+    ]
+
+    assert tokenize("  \n  hi  \n\n") == [
+        Token(loc=L, type='identifier', text="hi")
+    ]
+
+    assert tokenize(" hi number 1 ") == [
+        Token(loc=L, type='identifier', text="hi"),
+        Token(loc=L, type='identifier', text="number"),
+        Token(loc=L, type='int_literal', text="1")
+    ]
+
+    assert tokenize("3 + -5") == [
+        Token(loc=L, type="int_literal", text='3'),
+        Token(loc=L, type="operator", text='+'),
+        Token(loc=L, type="operator", text='-'),
+        Token(loc=L, type="int_literal", text='5'),
+    ]
+
+    assert tokenize("3 + -(1 + 2)") == [
+        Token(loc=L, type='int_literal', text='3'),
+        Token(loc=L, type='operator', text='+'),
+        Token(loc=L, type='operator', text='-'),
+        Token(loc=L, type='punctuation', text='('),
+        Token(loc=L, type='int_literal', text='1'),
+        Token(loc=L, type='operator', text='+'),
+        Token(loc=L, type='int_literal', text='2'),
+        Token(loc=L, type='punctuation', text=')'),
+    ]
+
+    assert tokenize("3-1") == [
+        Token(loc=L, type="int_literal", text='3'),
+        Token(loc=L, type="operator", text='-'),
+        Token(loc=L, type="int_literal", text='1'),
+    ]
+
+    assert tokenize("n >= 1") == [
+        Token(loc=L, type="identifier", text='n'),
+        Token(loc=L, type="operator", text='>='),
+        Token(loc=L, type="int_literal", text='1'),
+    ]
+
+    assert tokenize("print_int(3 * x);") == [
+        Token(loc=L, type="identifier", text='print_int'),
+        Token(loc=L, type="punctuation", text='('),
+        Token(loc=L, type="int_literal", text='3'),        
+        Token(loc=L, type="operator", text='*'),
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="punctuation", text=')'),
+        Token(loc=L, type="punctuation", text=';'),
+    ]
+
+    assert tokenize("x = x / 2") == [
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="operator", text='='),
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="operator", text='/'),
+        Token(loc=L, type="int_literal", text='2'),       
+    ]
+
+    assert tokenize("if x == 22 then {") == [
+        Token(loc=L, type="identifier", text='if'),
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="operator", text='=='),
+        Token(loc=L, type="int_literal", text='22'),
+        Token(loc=L, type="identifier", text='then'),
+        Token(loc=L, type="punctuation", text='{'),
+    ]
+
+    assert tokenize("// x < 11 \n x < 11") == [
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="operator", text='<'),
+        Token(loc=L, type="int_literal", text='11'),       
+    ]
+
+    assert tokenize("#\nx > 1") == [
+        Token(loc=L, type="identifier", text='x'),
+        Token(loc=L, type="operator", text='>'),
+        Token(loc=L, type="int_literal", text='1'),       
+    ]
+
+
+def test_tokenizer_location() -> None:
+
+    loc = SourceLocation(file="file_name", line=0, column=0)
+    assert tokenize("hello") == [
+        Token(loc=loc, type='identifier', text='hello')
+    ]
+
+    loc1 = SourceLocation(file="file_name", line=0, column=0)
+    loc2 = SourceLocation(file="file_name", line=2, column=0)
+    assert tokenize("hello\n\nworld") == [
+        Token(loc=loc1, type='identifier', text="hello"),
+        Token(loc=loc2, type='identifier', text="world"),
+    ]
+
+    loc1 = SourceLocation(file="file_name", line=0, column=0)
+    loc2 = SourceLocation(file="file_name", line=1, column=2)
+    assert tokenize("hello  \n  world") == [
+        Token(loc=loc1, type='identifier', text="hello"),
+        Token(loc=loc2, type='identifier', text="world"),
+    ]

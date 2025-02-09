1f4e3874f5c1ea69cb40e54fd30e65655b44de73
Anne Havunen <anne.havunen@helsinki.fi>
2025-02-09 12:50:18 +0200

    Add symbol table, variable declaration, blocks, and identifiers

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -1,17 +1,37 @@
-from typing import Any
+from dataclasses import dataclass, field
+from typing import Any, Optional
 from compiler import ast
+
+
+@dataclass
+class SymTab:
+    locals: dict = field(default_factory=dict)
+    parent: Optional['SymTab'] = None
+
+    def set(self, name: str, value: Any) -> None:
+        if name in self.locals:
+            raise Exception(f'Variable "{name}" already exists')
+        self.locals[name] = value
+
+    def get(self, name: str) -> Any:
+        if name in self.locals:
+            return self.locals[name]
+        elif self.parent:
+            return self.parent.get(name)
+        else:
+            raise Exception(f'Variable "{name}" is not defined')
 
 
 Value = int | bool | None
 
-def interpret(node: ast.Expression) -> Value:
+def interpret(node: ast.Expression, symtab: SymTab) -> Value:
     match node:
         case ast.Literal():
             return node.value
 
         case ast.BinaryOp():
-            a: Any = interpret(node.left)
-            b: Any = interpret(node.right)
+            a: Any = interpret(node.left, symtab)
+            b: Any = interpret(node.right, symtab)
             if node.op == '+':
                 return a + b 
             elif node.op == '-':
@@ -25,16 +45,33 @@ def interpret(node: ast.Expression) -> Value:
             else:
                 raise Exception(f'Unsupported operator "{node.op}"')
 
+        case ast.VarDeclaration():
+            value = interpret(node.value, symtab)
+            symtab.set(node.name, value)
+            return None
+
+        case ast.Identifier():
+            return symtab.get(node.name)
+
         case ast.IfExpression():
             if node.else_clause is not None:
-                if interpret(node.cond):
-                    return interpret(node.then_clause)
+                if interpret(node.cond, symtab):
+                    return interpret(node.then_clause, symtab)
                 else:
-                    return interpret(node.else_clause)
+                    return interpret(node.else_clause, symtab)
             else:
-                if interpret(node.cond):
-                    return interpret(node.then_clause)
+                if interpret(node.cond, symtab):
+                    interpret(node.then_clause, symtab)
                 return None
+
+        case ast.Block():
+            inner_scope = SymTab(parent=symtab)
+            result = None
+            for argument in node.arguments:
+                result = interpret(argument, inner_scope)
+            if isinstance(result, ast.Literal) and result.value is None:
+                return None
+            return result
 
         case _:
             raise Exception(f'Unsupported AST node: {node}')
--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -1,13 +1,32 @@
-from compiler.interpreter import interpret
+import pytest
+from compiler.interpreter import interpret, SymTab
 from compiler.parser import parse
 from compiler.tokenizer import tokenize
 
 
 def test_interpreter() -> None:
-    assert interpret(parse(tokenize('1 + 2'))) == 3
-    assert interpret(parse(tokenize('1 + 2 * 3'))) == 7
-    assert interpret(parse(tokenize('if 1 < 2 then 3 else 4'))) == 3
-    assert interpret(parse(tokenize('if 2 < 1 then 3 else 4'))) == 4
-    assert interpret(parse(tokenize('10 + if 2 < 1 then 3 else 4'))) == 14
-    assert interpret(parse(tokenize('if 1 < 2 then 3'))) == 3
-    assert interpret(parse(tokenize('if 2 < 1 then 3'))) == None
+    s = SymTab()
+    assert interpret(parse(tokenize('1 + 2')), s) == 3
+    assert interpret(parse(tokenize('1 + 2 * 3')), s) == 7
+    assert interpret(parse(tokenize('if 1 < 2 then 3 else 4')), s) == 3
+    assert interpret(parse(tokenize('if 2 < 1 then 3 else 4')), s) == 4
+    assert interpret(parse(tokenize('10 + if 2 < 1 then 3 else 4')), s) == 14
+    assert interpret(parse(tokenize('if 1 < 2 then 3')), s) == None
+    assert interpret(parse(tokenize('if 2 < 1 then 3')), s) == None
+    assert interpret(parse(tokenize('{}')), s) == None
+    assert interpret(parse(tokenize('{1}{2}')), s) == 2
+    assert interpret(parse(tokenize('{1}2')), s) == 2
+    assert interpret(parse(tokenize('{1}{2};')), s) == None
+    assert interpret(parse(tokenize('1 + 2;')), s) == None
+    assert interpret(parse(tokenize('var x = 10; x + 1')), s) == 11
+    assert interpret(parse(tokenize('var x = 10')), s) == None
+    assert interpret(parse(tokenize('var x = 10; {x}')), s) == 10
+    assert interpret(parse(tokenize('var x = 1; {var x = 2; x}')), s) == 2
+    assert interpret(parse(tokenize('var x = 1; {var x = 1; x}; var y = 2')), s) == None
+
+    s = SymTab()
+    with pytest.raises(Exception):
+        interpret(parse(tokenize('var x = 1; var x = 2')), s)
+    s = SymTab()
+    with pytest.raises(Exception):
+        interpret(parse(tokenize('x')), s)

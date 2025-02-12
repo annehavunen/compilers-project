b9862d8e1285ff675cd8f1a7327c2b321b82b6ed
Anne Havunen <anne.havunen@helsinki.fi>
2025-02-12 13:02:13 +0200

    Define operators and built-in functions in a symbol table (Interpreter Exercise 3)

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Optional
+from typing import Any, Optional, Callable
 from compiler import ast
 
 
@@ -9,8 +9,6 @@ class SymTab:
     parent: Optional['SymTab'] = None
 
     def set(self, name: str, value: Any) -> None:
-        if name in self.locals:
-            raise Exception(f'Variable "{name}" already exists')
         self.locals[name] = value
 
     def get(self, name: str) -> Any:
@@ -18,11 +16,88 @@ class SymTab:
             return self.locals[name]
         elif self.parent:
             return self.parent.get(name)
-        else:
-            raise Exception(f'Variable "{name}" is not defined')
+        return None
+    
+    def get_local(self, name: str) -> Any:
+        if name in self.locals:
+            return self.locals[name]
+        return None
 
+    def find_context(self, name: str) -> Optional['SymTab']:
+        if name in self.locals:
+            return self
+        if self.parent:
+            return self.parent.find_context(name)
+        return None
 
-Value = int | bool | None
+def build_toplevel_symtab() -> SymTab:
+    symtab = SymTab()
+
+    symtab.set('+', add)
+    symtab.set('-', subtract)
+    symtab.set('*', multiply)
+    symtab.set('/', divide)
+    symtab.set('%', modulo)
+    symtab.set('==', equals)
+    symtab.set('!=', not_equals)
+    symtab.set('<', less_than)
+    symtab.set('<=', less_than_or_equal)
+    symtab.set('>', greater_than)
+    symtab.set('>=', greater_than_or_equal)
+    symtab.set('print_int', print_int)
+    symtab.set('print_bool', print_bool)
+    symtab.set('read_int', read_int)
+    symtab.set('unary_-', unary_minus)
+    symtab.set('unary_not', unary_not)
+
+    return symtab
+
+def add(a: int, b: int) -> int:
+    return a + b
+def subtract(a: int, b: int) -> int:
+    return a - b
+def multiply(a: int, b: int) -> int:
+    return a * b
+def divide(a: int, b: int) -> int:
+    if b == 0:
+        raise Exception("Can't divide by zero")
+    return a // b
+def modulo(a: int, b: int) -> int:
+    return a % b
+def equals(a: int | bool | None, b: int | bool | None) -> bool:
+    return a == b
+def not_equals(a: int | bool | None, b: int | bool | None) -> bool:
+    return a != b
+def less_than(a: int | bool, b: int | bool) -> bool:
+    return a < b
+def less_than_or_equal(a: int | bool, b: int | bool) -> bool:
+    return a <= b
+def greater_than(a: int | bool, b: int | bool) -> bool:
+    return a > b
+def greater_than_or_equal(a: int | bool, b: int | bool) -> bool:
+    return a >= b
+def unary_minus(a: int) -> int:
+    if type(a) != int:
+        raise Exception(f'Value {a} was not integer')
+    return -1 * a
+def unary_not(a: bool) -> bool:
+    if type(a) != bool:
+        raise Exception(f'Value {a} was not boolean')
+    return not a
+def print_int(a: int) -> None:
+    print(a)
+    return None
+def print_bool(a: bool) -> None:
+    if a:
+        print('true')
+    else:
+        print('false')
+    return None
+def read_int() -> int:
+    return int(input())
+
+Value = int | bool | None | Callable
+
 
 def interpret(node: ast.Expression, symtab: SymTab) -> Value:
     match node:
@@ -30,28 +105,34 @@ def interpret(node: ast.Expression, symtab: SymTab) -> Value:
             return node.value
 
         case ast.BinaryOp():
+            if node.op == "=":
+                if not isinstance(node.left, ast.Identifier):
+                    raise Exception('Left of assignment must be an identifier')
+                name = node.left.name
+                value = interpret(node.right, symtab)
+                context = symtab.find_context(name)
+                if context is None:
+                    raise Exception(f'Variable "{name}" is not set')
+                context.set(name, value)
+                return value
+
             a: Any = interpret(node.left, symtab)
             b: Any = interpret(node.right, symtab)
-            if node.op == '+':
-                return a + b 
-            elif node.op == '-':
-                return a - b
-            elif node.op == '*':
-                return a * b
-            elif node.op == '/':
-                return a // b
-            elif node.op == '<':
-                return a < b
-            else:
-                raise Exception(f'Unsupported operator "{node.op}"')
+            binaryop = symtab.get(node.op)
+            return binaryop(a, b)
 
         case ast.VarDeclaration():
+            if symtab.get_local(node.name) is not None:
+                raise Exception(f'Value for "{node.name}" already exists')
             value = interpret(node.value, symtab)
             symtab.set(node.name, value)
             return None
 
         case ast.Identifier():
-            return symtab.get(node.name)
+            value = symtab.get(node.name)
+            if value is None:
+                raise Exception(f'Variable "{node.name}" is not set')
+            return value
 
         case ast.IfExpression():
             if node.else_clause is not None:
@@ -72,6 +153,22 @@ def interpret(node: ast.Expression, symtab: SymTab) -> Value:
             if isinstance(result, ast.Literal) and result.value is None:
                 return None
             return result
+
+        case ast.FunctionCall():
+            func = symtab.get(node.name)
+            args = []
+            for argument in node.arguments:
+                args.append(interpret(argument, symtab))
+            if node.name in ['print_int', 'print_bool'] and len(args) != 1:
+                raise Exception(f'Wrong number of arguments in {node.name}')
+            if node.name == 'read_int' and len(args) != 0:
+                raise Exception(f'Wrong number of arguments in {node.name}')
+            return func(*args)
+
+        case ast.UnaryOp():
+            value = interpret(node.exp, symtab)
+            unaryop = symtab.get(f'unary_{node.op}')
+            return unaryop(value)
 
         case _:
             raise Exception(f'Unsupported AST node: {node}')
--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -1,13 +1,17 @@
 import pytest
-from compiler.interpreter import interpret, SymTab
+from compiler.interpreter import interpret, build_toplevel_symtab
 from compiler.parser import parse
 from compiler.tokenizer import tokenize
 
 
 def test_interpreter() -> None:
-    s = SymTab()
+    s = build_toplevel_symtab()
     assert interpret(parse(tokenize('1 + 2')), s) == 3
     assert interpret(parse(tokenize('1 + 2 * 3')), s) == 7
+    assert interpret(parse(tokenize('4 / -2')), s) == -2
+    assert interpret(parse(tokenize('4 % 2')), s) == 0
+    assert interpret(parse(tokenize('true == not false')), s) == True
+    assert interpret(parse(tokenize('(1 + 1) >= 2')), s) == True
     assert interpret(parse(tokenize('if 1 < 2 then 3 else 4')), s) == 3
     assert interpret(parse(tokenize('if 2 < 1 then 3 else 4')), s) == 4
     assert interpret(parse(tokenize('10 + if 2 < 1 then 3 else 4')), s) == 14
@@ -22,11 +26,22 @@ def test_interpreter() -> None:
     assert interpret(parse(tokenize('var x = 10')), s) == None
     assert interpret(parse(tokenize('var x = 10; {x}')), s) == 10
     assert interpret(parse(tokenize('var x = 1; {var x = 2; x}')), s) == 2
-    assert interpret(parse(tokenize('var x = 1; {var x = 1; x}; var y = 2')), s) == None
+    assert interpret(parse(tokenize('var x = 1; {x = 2}; x')), s) == 2
+    assert interpret(parse(tokenize('var x = 1; x = 2; x')), s) == 2
+    assert interpret(parse(tokenize('var x = 1; {var x = 2}; x')), s) == 1
+    assert interpret(parse(tokenize('var x = (1 + 2); var y = x; y')), s) == 3
 
-    s = SymTab()
+    s = build_toplevel_symtab()
     with pytest.raises(Exception):
         interpret(parse(tokenize('var x = 1; var x = 2')), s)
-    s = SymTab()
+    s = build_toplevel_symtab()
     with pytest.raises(Exception):
         interpret(parse(tokenize('x')), s)
+    s = build_toplevel_symtab()
+    with pytest.raises(Exception):
+        interpret(parse(tokenize('{var x = 1}; x')), s)
+    s = build_toplevel_symtab()
+    with pytest.raises(Exception):
+        interpret(parse(tokenize('{{var x = 1}; x}')), s)
+    with pytest.raises(Exception):
+        interpret(parse(tokenize('{var x = 1}{x}')), s)

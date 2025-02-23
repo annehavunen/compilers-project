7548d65f9019a96df51b589aa7830a0f8b8d4c83
Anne Havunen <anne.havunen@helsinki.fi>
2025-02-23 10:04:30 +0200

    Implement remaining operators, variables, blocks, and while expression

--- src/compiler/type_checker.py
+++ src/compiler/type_checker.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass, field
 from typing import Optional, Any
 
 
+UNDEFINED = object()
+
 @dataclass
 class TypeSymTab:
     locals: dict = field(default_factory=dict)
@@ -18,7 +20,19 @@ class TypeSymTab:
             return self.locals[name]
         elif self.parent:
             return self.parent.get(name)
-        return None
+        return UNDEFINED
+
+    def get_local(self, name: str) -> Any:
+        if name in self.locals:
+            return self.locals[name]
+        return UNDEFINED
+
+    def find_scope(self, name: str) -> Any:
+        if name in self.locals:
+            return self
+        if self.parent:
+            return self.parent.find_scope(name)
+        return UNDEFINED
 
 def build_type_symtab() -> TypeSymTab:
     symtab = TypeSymTab()
@@ -34,6 +48,8 @@ def build_type_symtab() -> TypeSymTab:
     symtab.set('>=', FunType([Int, Int], Bool))
     symtab.set('and', FunType([Bool, Bool], Bool))
     symtab.set('or', FunType([Bool, Bool], Bool))
+    symtab.set('unary_-', FunType([Int], Int))
+    symtab.set('unary_not', FunType([Bool], Bool))
     symtab.set('print_int', FunType([Int], Unit))
     symtab.set('print_bool', FunType([Bool], Unit))
     symtab.set('read_int', FunType([], Int))
@@ -48,6 +64,8 @@ def typecheck(node: ast.Expression, symtab: TypeSymTab) -> Type:
                 return Bool
             elif isinstance(node.value, int):
                 return Int
+            elif node.value is None:
+                return Unit
             else:
                 raise Exception(f"Don't know type of literal: {node.value}")
 
@@ -55,13 +73,57 @@ def typecheck(node: ast.Expression, symtab: TypeSymTab) -> Type:
             t1 = typecheck(node.left, symtab)
             t2 = typecheck(node.right, symtab)
 
+            if node.op in ['==', '!=']:
+                if t1 != t2:
+                    raise Exception(f"Operator '{node.op} had different types: {t1} and {t2}")
+                return Bool
+
+            elif node.op == '=':
+                if t1 != t2:
+                    raise Exception(f"Operator '{node.op} had different types: {t1} and {t2}")
+                elif not isinstance(node.left, ast.Identifier):
+                    raise Exception("Left of assignment must be an identifier")
+                name = node.left.name
+                scope = symtab.find_scope(name)
+                if scope is UNDEFINED:
+                    raise Exception(f'Variable "{name}" is not set')
+                scope.set(name, t2)
+                return t2
+
             binaryop = symtab.get(node.op)
-            if not binaryop:
+            if binaryop is UNDEFINED:
                 raise Exception(f"Unknown operator: {node.op}")
 
-            if t1 != binaryop.arg_types[0] or t2 != binaryop.arg_types[1]:
+            elif t1 != binaryop.arg_types[0] or t2 != binaryop.arg_types[1]:
                 raise Exception(f"Unexpected types with operator '{node.op}', got '{t1}' and '{t2}'")
             return binaryop.return_type
+
+        case ast.UnaryOp():
+            type = typecheck(node.expr, symtab)
+            unaryop = symtab.get(f'unary_{node.op}')
+            if type != unaryop.arg_types[0]:
+                raise Exception(f"Unary operator '{node.op}' expected type '{unaryop.arg_types[0]}', got '{type}'")
+            return type         
+
+        case ast.VarDeclaration():
+            if symtab.get_local(node.name) is not UNDEFINED:
+                raise Exception(f"Value for '{node.name}' already exists")
+            type = typecheck(node.value, symtab)
+            symtab.set(node.name, type)
+            return Unit
+
+        case ast.Identifier():
+            type = symtab.get(node.name)
+            if type is UNDEFINED:
+                raise Exception(f"Variable '{node.name}' is not set")
+            return type
+
+        case ast.Block():
+            inner_scope = TypeSymTab(parent=symtab)
+            return_type: Any = Unit
+            for argument in node.arguments:
+                return_type = typecheck(argument, inner_scope)
+            return return_type
 
         case ast.IfExpression():
             t1 = typecheck(node.cond, symtab)
@@ -74,6 +136,12 @@ def typecheck(node: ast.Expression, symtab: TypeSymTab) -> Type:
             if t2 != t3:
                 raise Exception(f"'then' and 'else' had different types: {t2} and {t3}")
             return t2
+
+        case ast.WhileExpression():
+            cond_type = typecheck(node.cond, symtab)
+            if cond_type is not Bool:
+                raise Exception(f"'while' condition was '{cond_type}'")
+            return Unit
 
         case ast.FunctionCall():
             func = symtab.get(node.name)
--- tests/type_checker_test.py
+++ tests/type_checker_test.py
@@ -18,21 +18,53 @@ def test_type_checker() -> None:
     assert typecheck(parse(tokenize('2 >= 3 or 1 <= 2')), t) == Bool
     assert typecheck(parse(tokenize('true and false')), t) == Bool
     assert typecheck(parse(tokenize('if 1 < 2 then 3')), t) == Unit
-    assert typecheck(parse(tokenize('if 1 < 2 then 3 else 4')), t) == Int
+    assert typecheck(parse(tokenize('1 + if 1 < 2 then 3 else 4')), t) == Int
     assert typecheck(parse(tokenize('if 1 < 2 then 3 < 4 else 4 < 5')), t) == Bool
+    assert typecheck(parse(tokenize('1 + if 1 < 2 then 3 else 4')), t) == Int
     assert typecheck(parse(tokenize('print_int(1)')), t) == Unit
     assert typecheck(parse(tokenize('print_bool(false)')), t) == Unit
     assert typecheck(parse(tokenize('read_int()')), t) == Int
+    assert typecheck(parse(tokenize('var x = 1')), t) == Unit
+    assert typecheck(parse(tokenize('var y = -1 + 2')), t) == Unit
+    assert typecheck(parse(tokenize('var z = 2; z')), t) == Int
+    assert typecheck(parse(tokenize('var a = if true then 1; a')), t) == Unit
+    assert typecheck(parse(tokenize('var b = if true then 1 else 2; b')), t) == Int
+    assert typecheck(parse(tokenize('-2')), t) == Int
+    assert typecheck(parse(tokenize('not (1 < 2)')), t) == Bool
+    assert typecheck(parse(tokenize('- if 1 < 2 then 3 else 4')), t) == Int
+    assert typecheck(parse(tokenize('1 == 2')), t) == Bool
+    assert typecheck(parse(tokenize('1 < 2 != 2 < 3')), t) == Bool
+    assert typecheck(parse(tokenize('1;')), t) == Unit
+    assert typecheck(parse(tokenize('1;2')), t) == Int
+    assert typecheck(parse(tokenize('{}')), t) == Unit
+    t = build_type_symtab()
+    assert typecheck(parse(tokenize('{var a = 1} var a = 2')), t) == Unit
+    assert typecheck(parse(tokenize('var a = 1; {var a = 2}')), t) == Unit
+    assert typecheck(parse(tokenize('var b = {var c = true; c}; b')), t) == Bool
+    assert typecheck(parse(tokenize('var c = 2; c = 3')), t) == Int
+    assert typecheck(parse(tokenize('var x = {}; x = if true then 2')), t) == Unit
+    assert typecheck(parse(tokenize('var y = while true do y + 1; y')), t) == Unit
+    assert typecheck(parse(tokenize('while {var z = 1 > 2; z} do 1')), t) == Unit
 
+    t = build_type_symtab()
     assert_fails_typecheck('(1 < 3) + 3', t)
     assert_fails_typecheck('true < 3', t)
     assert_fails_typecheck('true and 3', t)
     assert_fails_typecheck('1 or 3', t)
+    assert_fails_typecheck('-false', t)
+    assert_fails_typecheck('not 1', t)
+    assert_fails_typecheck('true == 1', t)
     assert_fails_typecheck('if 1 then 3 else 4', t)
     assert_fails_typecheck('if 1 < 2 then 3 else 4 < 5', t)
+    assert_fails_typecheck('1 + if true then 2', t)
     assert_fails_typecheck('print_int(true)', t)
     assert_fails_typecheck('print_bool(1)', t)
     assert_fails_typecheck('read_int(1)', t)
+    assert_fails_typecheck('var x = 1; var x = 2', t)
+    assert_fails_typecheck('var y = {}; var y = 2', t)
+    assert_fails_typecheck('z = 2', t)
+    assert_fails_typecheck('1 = 2', t)
+    assert_fails_typecheck('while 2 do 1', t)
 
 def assert_fails_typecheck(code: str, t: TypeSymTab) -> None:
     expr = parse(tokenize(code))
